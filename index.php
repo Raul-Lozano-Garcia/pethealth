fa573897921050ea89b6352761ef8955e00626d9
Raúl Lozano García <59658847+Raul-Lozano-Garcia@users.noreply.github.com>
2022-02-01 11:45:53 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
     <title>PETHEALTH - Tu veterinaria de confianza</title>
     <script type="text/javascript" src="js/app.js" defer></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="icon" href="assets/imagenes/logo_negro.png" type="image/png"/>
     <link rel="stylesheet" href="css/estilos.css">
 </head>
@@ -97,6 +98,16 @@
                 }
 
             ?>
+            </div>
+        </section>
+        <!-- GALERÍA -->
+        <section id="galeria" class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Galería</h2>
+                </div>
+            </div>
+            <div id="fotos" class="row">
             </div>
         </section>
         <section id="testimonio">
--- js/app.js
+++ js/app.js
@@ -2,9 +2,31 @@
 
 const hamburguesa = document.querySelector('#hamburguesa');
 const nav= document.querySelector("nav");
+const galeria = document.querySelector("#fotos");
 
-// Abrir y cerrar nav y movil
-hamburguesa.addEventListener('click', () => {
-    hamburguesa.classList.toggle('nav-open');
-    nav.classList.toggle('nav-open');
-});
+//PREGUNTAR DANI SI QUIERO QUE CARGUE ASI, SIN DARLE A NINGUN BOTON
+window.onload= async function () {
+  const respuesta = await fetch("https://dog.ceo/api/breeds/image/random/6");
+  const datos = await respuesta.json();
+
+  const lista_perros = datos["message"];
+  lista_perros.forEach(
+    (perro) => {
+      galeria.appendChild(crearFila(perro));
+    });
+};
+
+
+const crearFila = (imagen) => {
+  const caja = document.createElement("div");
+  caja.classList.add("col-12","col-md-4");
+
+  const ima = document.createElement("img");
+  ima.src = imagen;
+  ima.classList.add("w-100","shadow-1-strong","rounded","mb-4","img-fluid");
+  ima.style.height="250px";
+
+  caja.appendChild(ima);
+
+  return caja;
+}
